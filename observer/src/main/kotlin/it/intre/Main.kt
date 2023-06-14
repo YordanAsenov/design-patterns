ca73768a6150ce6997ee14f99d1fa54310a58f17
Marcato Luca <marcato@intre.it>
2023-06-14 17:52:00 +0200

    :clown_face: unsubscribe obeserver from subject

--- observer/src/main/kotlin/it/intre/Main.kt
+++ observer/src/main/kotlin/it/intre/Main.kt
@@ -2,13 +2,15 @@ package it.intre
 fun main() {
     println("Pull weather station: ")
     val pullWeatherData = it.intre.pull.weather.WeatherData()
-    it.intre.pull.weather.TemperatureDisplay(pullWeatherData)
+    val pullTemperatureDisplay = it.intre.pull.weather.TemperatureDisplay(pullWeatherData)
     pullWeatherData.updateMeasurements(80.0f, 65.0f, 30.4f)
     pullWeatherData.updateMeasurements(2.0f, 6.0f, 3.0f)
+    pullWeatherData.removeObserver(pullTemperatureDisplay)
 
     println("Push weather station: ")
     val pushWeatherData = it.intre.push.weather.WeatherData()
-    it.intre.push.weather.TemperatureDisplay(pushWeatherData)
+    val pushTemperatureDisplay = it.intre.push.weather.TemperatureDisplay(pushWeatherData)
     pushWeatherData.updateMeasurements(80.0f, 65.0f, 30.4f)
     pushWeatherData.updateMeasurements(2.0f, 6.0f, 3.0f)
+    pushWeatherData.removeObserver(pushTemperatureDisplay)
 }
